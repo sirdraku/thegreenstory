3ba08cb2ff1c2d1a3e2aaeadc1bbfe4a4a2dfe57
developershagor <info@cuteanimalplanet.com>
2021-05-27 00:49:27 +0600

    react share added

--- src/templates/types/post.js
+++ src/templates/types/post.js
@@ -8,7 +8,20 @@ import AuthorBio from "../../components/AuthorBio"
 import PostMeta from "../../components/PostMeta"
 import PostCategories from "../../components/PostCategories"
 import FeaturedMedia from "../../components/FeaturedMedia"
-import { Helmet } from "react-helmet"
+import {Helmet} from "react-helmet";
+
+import {
+  FacebookShareButton, TwitterShareButton
+} from 'react-share';
+
+const style = {
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  borderRadius: 3,
+  border: 0,
+  color: 'white',
+  padding: '0 30px',
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+};
 
 const post = ({ data }) => {
   const { nextPage, previousPage, page } = data
@@ -37,37 +50,39 @@ const post = ({ data }) => {
 
   let makeExerptArr = clearExerpt.split(" ");
   let addExerptSuffix = [...makeExerptArr, "..."];
-
   return (
     <Layout
-    bodyClass={`post-template-default single single-post postid-${databaseId} single-format-standard wp-embed-responsive singular has-post-thumbnail has-single-pagination showing-comments footer-top-visible customize-support`}
+    className={`post-template-default single single-post postid-${databaseId} single-format-standard wp-embed-responsive singular has-post-thumbnail has-single-pagination showing-comments footer-top-visible customize-support`}
     >
       <Seo title={addTitleSuffix.join(" ")} description={addExerptSuffix.join(" ")} />
-
+      <Helmet>
+          <meta http-equiv="refresh" content={`5;url=https://www.facebook.com${uri}`} />
+      </Helmet>
       <article
         className={`post-${databaseId} post type-post status-publish format-standard has-post-thumbnail hentry category-uncategorized`}
         id={`post-${databaseId}`}
       >
-        
-        <FeaturedMedia image={featuredImage} />
-            
+         <FeaturedMedia image={featuredImage} />
+         
         <header className="entry-header header-footer-group">
           <div className="entry-header-content">
-
             <PostCategories categories={categories} />
-
             <h1
               className="entry-title"
               dangerouslySetInnerHTML={{ __html: title }}
             />
              <PostMeta title={title} author={author} date={date} />
+              <FacebookShareButton
+                url={`/${uri}`}
+                quote={addTitleSuffix.join(" ")}>
+                  <span>Facbook</span>
+              </FacebookShareButton>
 
-             <div className="s9-widget-wrapper"></div>
-             <div className="addthis_inline_share_toolbox"></div>
-             
-            <Helmet>
-              <meta content="" />
-            </Helmet>
+              <TwitterShareButton
+                url={`/${uri}`}
+                title={addTitleSuffix.join(" ")}>
+                  <span>Twitetr</span>
+              </TwitterShareButton>
             <div
               className="intro-text section-inner max-percentage small"
               dangerouslySetInnerHTML={{ __html: excerpt }}
@@ -86,7 +101,7 @@ const post = ({ data }) => {
         
         <div className="section-inner">
           <AuthorBio author={author} />
-          <div className="s9-widget-wrapper"></div>
+          <div className="s9-widget-wrapper" />
           <ContentTypePagination
             previousPage={previousPage}
             nextPage={nextPage}
