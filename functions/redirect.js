8045b0e406218ebccbe1f07a79534580e5a87806
develop <info@cuteanimalplanet.com>
2021-06-02 01:29:00 +0600

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,18 +1,8 @@
 exports.handler = async event => {
-    if (event.headers.referer.includes('facebook')) {
-      return {
-        statusCode: 301,
-        headers: {
-          location: 'https://www.google.com/'
-        }
-      }
-    } else {
-      let pathName = location.pathname.split('/')[2].split('-')
-      return {
-        statusCode: 301,
-        headers: {
-          location: process.env.URL + pathName[0] + '/' + pathName[1]
-        }
-      }
+  return {
+    statusCode: 301,
+    headers: {
+      location: decodeURIComponent(event.queryStringParameters.url.replace(/\+/g, '%20'))
     }
   }
+}
