da3978f8e22b6a8f5eac3b2c406350b2a759dd85
develop <info@cuteanimalplanet.com>
2021-06-04 19:06:51 +0600

    Update redirect.js

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,20 +1,8 @@
 exports.handler = async event => {
-  if (event.queryStringParameters.fbclid) {
-    let pathName = event.path.split('/')[3].split('-')
-    return {
-      statusCode: 301,
-      headers: {
-        'cache-control': 'public, max-age=0, must-revalidate',
-        location: process.env.URL + '/' + pathName[0] + '/' + pathName[1] + '/'
-      }
-    }
-  } else {
-    return {
-      statusCode: 301,
-      headers: {
-        'cache-control': 'public, max-age=0, must-revalidate',
-        location: 'https://www.cuteanimalplanet.com/'
-      }
+  return {
+    statusCode: 301,
+    headers: {
+      location: decodeURIComponent(event.queryStringParameters.url.replace(/\+/g, '%20'))
     }
   }
 }
